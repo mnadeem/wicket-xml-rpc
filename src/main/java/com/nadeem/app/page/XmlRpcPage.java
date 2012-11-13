da7836d33c3be9cedbfb77a60e5f1477b76fb1df
Nadeem Mohammad <coolmind182006@gmail.com>
2012-11-13 18:30:07 +0530

    refactored

--- src/main/java/com/nadeem/app/page/XmlRpcPage.java
+++ src/main/java/com/nadeem/app/page/XmlRpcPage.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.IBehavior;
 import org.apache.wicket.markup.MarkupStream;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebRequest;
 import org.apache.wicket.protocol.http.WebResponse;
+import org.apache.xmlrpc.webserver.XmlRpcServletServer;
 
 import com.nadeem.app.WicketApplication;
 
@@ -16,8 +19,15 @@ public final class XmlRpcPage extends WebPage {
 	public XmlRpcPage() throws ServletException, IOException {
 		setStatelessHint(true);
 
-		((WicketApplication) getApplication()).getXmlRpcServer().execute(((WebRequest)getRequestCycle().getRequest()).getHttpServletRequest(),
-													((WebResponse)getRequestCycle().getResponse()).getHttpServletResponse());
+		processXMLRpcRequest(((WebRequest) getRequestCycle().getRequest()), ((WebResponse) getRequestCycle().getResponse()));
+	}
+
+	private void processXMLRpcRequest(final WebRequest request, final WebResponse response) throws ServletException, IOException {
+		getXMLRpcServer().execute(request.getHttpServletRequest(), response.getHttpServletResponse());
+	}
+
+	private XmlRpcServletServer getXMLRpcServer() {
+		return ((WicketApplication) getApplication()).getXmlRpcServer();
 	}
 
 	@Override
@@ -25,12 +35,17 @@ public final class XmlRpcPage extends WebPage {
 		return false;
 	}
 
-	public final String getMarkupType() {
+	public String getMarkupType() {
 		return "text/xml";
 	}
 
 	@Override
-	protected void onRender(MarkupStream markupStream) {
+	protected void onRender(final MarkupStream markupStream) {
 
+	}
+
+	@Override
+	public Component add(final IBehavior... behaviors) {
+		throw new UnsupportedOperationException("XmlRpcPage does not support IBehaviours");
 	}
 }
